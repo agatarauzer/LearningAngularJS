53733fc005791eac0bb6e56ff1ee37d1d2a72a0f
agatarauzer <agatarauzer@gmail.com>
2018-09-28 20:18:23 +0200

    adding, editing and displaying users; adding and displaying orders

--- KuviCourseAngularJS/ngShop/js/application.js
+++ KuviCourseAngularJS/ngShop/js/application.js
@@ -5,6 +5,8 @@ var application = angular.module( 'ngShop' , [ 'ngRoute' , 'appControllers' ] );
 
 application.config( [ '$routeProvider' , '$httpProvider' , function( $routeProvider , $httpProvider ) {
 
+	//====== products =====
+
 	$routeProvider
 	.when( '/products' , {
 		controller : 'products',
@@ -20,6 +22,32 @@ application.config( [ '$routeProvider' , '$httpProvider' , function( $routeProvi
 		controller : 'productCreate',
 		templateUrl : 'partials/product-create.html'
 	})
+
+	//===== users =====
+
+	.when( '/users' , {
+		controller : 'users' ,
+		templateUrl : 'partials/users.html'
+	})
+
+	.when( '/user/edit/:id' , {
+		controller : 'userEdit',
+		templateUrl : 'partials/user-edit.html'
+	})
+
+	.when( '/user/create' , {
+		controller : 'userCreate',
+		templateUrl : 'partials/user-create.html'
+	})
+
+	//===== orders =====
+
+	.when( '/orders' , {
+		controller : 'orders' ,
+		templateUrl : 'partials/orders.html'
+	})
+
+	//===== default =====
 
 	.otherwise({
 		redirectTo: '/home'
--- KuviCourseAngularJS/ngShop/js/controllers.js
+++ KuviCourseAngularJS/ngShop/js/controllers.js
@@ -3,6 +3,17 @@
 var appControllers = angular.module( 'appControllers' , [ 'ngRoute' ]);
 
 
+appControllers.controller( 'appNavigation' , [ '$scope' , '$location' , function( $scope , $location ) {
+
+	console.log( $location.path());
+
+	$scope.isActive = function( path ) {
+		return $location.path() === path;
+	};
+
+}]);
+
+
 appControllers.controller( 'products' , [ '$scope' , '$http' , function( $scope , $http ) {
 
 	$http.get( 'model/products.json' ).then( successCallback, errorCallback);
@@ -55,4 +66,86 @@ appControllers.controller( 'productCreate' , [ '$scope' , '$http' , function( $s
 		console.log( $scope.product );
 	};
 
+}]);
+
+appControllers.controller( 'users' , [ '$scope' , '$http' , function( $scope , $http ) {
+	
+	$http.get( 'model/users.json' ).then( successCallback, errorCallback);
+
+	function successCallback( response ) {
+		$scope.users = response.data;
+	};
+
+	function errorCallback(){
+		console.log( 'Błąd pobrania pliku json' )
+	};
+
+	$scope.deleteUser = function( user , $index ) {
+		//TODO: connect with API 
+
+		$scope.users.splice( $index , 1 );
+	};
+}]);
+
+appControllers.controller( 'userEdit' , [ '$scope' , '$http' , '$routeParams' , function( $scope , $http , $routeParams ) {
+
+	$http.get( 'model/users.json' ).then( successCallback, errorCallback);
+
+	function successCallback( response ) {
+		var users = response.data;
+		$scope.user = users[$routeParams.id]
+	};
+
+	function errorCallback(){
+		console.log( 'Błąd pobrania pliku json' )
+	};
+
+	$scope.saveChanges = function( user) {
+		//TODO: connect with API 
+
+		console.log( user );
+		console.log( $routeParams.id);
+		};
+
+}]);
+
+appControllers.controller( 'userCreate' , [ '$scope' , '$http' , function( $scope , $http ) {
+
+	$scope.createUser = function() {
+
+		//TODO: connect with API 
+
+		console.log( $scope.user );
+	};
+
+}]);
+
+appControllers.controller( 'orders' , [ '$scope' , '$http' , function( $scope , $http ) {
+	
+	$http.get( 'model/orders.json' ).then( successCallback, errorCallback);
+
+	function successCallback( response ) {
+		$scope.orders = response.data;
+	};
+
+	function errorCallback(){
+		console.log( 'Błąd pobrania pliku json' )
+	};
+
+	$scope.deleteOrder = function( order , $index ) {
+		//TODO: connect with API 
+
+		$scope.orders.splice( $index , 1 );
+	};
+
+	$scope.changeStatus = function( order) {
+
+		if (order.status == 0) {
+			order.status = 1;
+		}
+		else {
+			order.status = 0;
+		}
+		
+	};
 }]);
